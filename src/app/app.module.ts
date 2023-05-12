e3ae2d764e99ce061f9c115a55b4c9a5577e0389
hamdanimed <hamdanimee@gmail.com>
2023-05-12 21:22:11 +0100

    minor changes , need responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreationFormComponent } from './creation-form/creation-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreationFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/creation-form/creation-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creation-form',
+  templateUrl: './creation-form.component.html',
+  styleUrls: ['./creation-form.component.css']
+})
+export class CreationFormComponent {
+
+}
