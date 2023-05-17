19ef4b540b9f733db475f3076f5a292b5eeb767d
hamdanimed <hamdanimee@gmail.com>
2023-05-17 14:03:06 +0100

    login component

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+
+  constructor(public router:Router){}
+  login(){
+    this.router.navigate(['/create-agent'])
+  }
 
 }
